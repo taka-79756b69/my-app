8352d2c99290dbf0e69aeb97ce2a70a847cc244e
taka-79756b69 <taka.x02@gmail.com>
2023-01-07 02:12:32 +0900

    コース60まで進んだ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
