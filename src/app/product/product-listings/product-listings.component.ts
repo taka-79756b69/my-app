2b1e4c72c35595105bc3de2b4f30f3de9339a8ab
taka-79756b69 <taka.x02@gmail.com>
2023-01-10 01:51:36 +0900

    フロントエンド側を作成

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from 'src/app/product';
 
 @Component({
   selector: 'app-product-listings',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-listings.component.scss']
 })
 export class ProductListComponent {
-  products: any = [1, 2, 3, 4]
+
+  products: any
+
+  ngOnInit(): void {
+    this.products = products;
+  }
 }
