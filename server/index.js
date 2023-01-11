3069fd86ee52b31c11a90f65e2de22034311f3cc
taka-79756b69 <taka.x02@gmail.com>
2023-01-12 01:30:03 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.set('strictQuery', false)
 
@@ -12,14 +13,24 @@ mongoose.connect(config.DB_URI, {
   useUnifiedTopology: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
   }
 )
 
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'my-app')
+  app.use(express.static(appPath))
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || '3001'
 
